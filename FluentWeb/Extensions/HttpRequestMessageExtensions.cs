357c98358c7925aceebc83d1d4de1c89266e00a6
Konstantin Ivanov <exellion.registr@gmail.com>
2020-10-01 11:11:18 +0300

    initial commit

---
+++ FluentWeb/Extensions/HttpRequestMessageExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace FluentWeb
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static HttpRequestMessage WithParameter(this HttpRequestMessage message, string name, object value)
+        {
+            if (value == null)
+                return message;
+            
+            string url = message.RequestUri.ToString();
+
+            if (url.Contains("?"))
+                url += "&";
+            else
+                url += "?";
+
+            url = $"{url}{name}={value}";
+
+            message.RequestUri = new Uri(url, UriKind.Relative);
+            
+            return message;
+        }
+        
+        public static HttpRequestMessage WithHeader(this HttpRequestMessage message, string name, string value)
+        {
+            if (!String.IsNullOrEmpty(value))
+                message.Headers.Add(name, value);
+            
+            return message;
+        }
+        
+        public static HttpRequestMessage WithJsonBody(this HttpRequestMessage message, object body, JsonSerializerSettings serializerSettings = null)
+        {
+            message.Content = new StringContent(JsonConvert.SerializeObject(body, serializerSettings!), Encoding.UTF8, "application/json");
+            return message;
+        }
+    }
+}
---
+++ FluentWeb/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+
+namespace FluentWeb
+{
+    public static class StringExtensions
+    {
+        public static HttpRequestMessage Get(this string endpoint) => new HttpRequestMessage(HttpMethod.Get, endpoint);
+        
+        public static HttpRequestMessage Post(this string endpoint) => new HttpRequestMessage(HttpMethod.Post, endpoint);
+        
+        public static HttpRequestMessage Put(this string endpoint) => new HttpRequestMessage(HttpMethod.Put, endpoint);
+        
+        public static HttpRequestMessage Patch(this string endpoint) => new HttpRequestMessage(new HttpMethod("PATCH"), endpoint);
+        
+        public static HttpRequestMessage Delete(this string endpoint) => new HttpRequestMessage(HttpMethod.Delete, endpoint);
+        
+        public static HttpRequestMessage Head(this string endpoint) => new HttpRequestMessage(HttpMethod.Head, endpoint);
+        
+        public static HttpRequestMessage Options(this string endpoint) => new HttpRequestMessage(HttpMethod.Options, endpoint);
+        
+        public static HttpRequestMessage Trace(this string endpoint) => new HttpRequestMessage(HttpMethod.Trace, endpoint);
+    }
+}
